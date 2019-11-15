e8caf5c456ae9af5424f970628612d587d4eaa8b
Jonahlyn Gilstrap <jonahlyn@unm.edu>
2019-11-15 22:30:23 +0000

    Accessibility improvements including increased font size and color contrast of the menu, remove empty link in the logo, adding outlines to links for keyboard navigation, added missing alt text in the slide images and enabled pause on hover.

---
+++ assets/js/slide.js
@@ -0,0 +1,10 @@
+jQuery(window).on('load', function() {
+
+  jQuery('#slidebox').flexslider({
+    animation: "fade",
+    directionNav:true,
+    controlNav:false,
+    pauseOnHover: true
+  });
+
+});
